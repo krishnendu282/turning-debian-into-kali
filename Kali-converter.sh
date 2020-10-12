c2802f13dcc47d78243899f0cc60417230499f99
krishnendu282 <50852745+krishnendu282@users.noreply.github.com>
2020-10-12 12:04:24 +0530

    Update Kali-converter.sh

--- Kali-converter.sh
+++ Kali-converter.sh
@@ -1,4 +1,4 @@
 apt update && apt upgrade -y
-apt install wget gnupg2 -y
+apt install gnupg2 -y
 echo "deb https://http.kali.org/kali kali-rolling main non-free contrib" | tee /etc/apt/sources.list
 wget -q -O - https://archive.kali.org/archive-key.asc | apt-key add
